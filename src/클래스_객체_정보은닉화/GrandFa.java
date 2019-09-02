76eac8d99a46cb898a8c9579e7a74fa4618df501
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-03 08:11:35 +0900

    First day

---
+++ src/클래스_객체_정보은닉화/GrandFa.java
@@ -0,0 +1,31 @@
+package 클래스_객체_정보은닉화;
+
+public class GrandFa {
+	//클래스에 new해서 객체로 만드는 것을 '인스턴스화' 라고 한다.
+	//예 ) Scanner sc; <<클래스
+	//예 ) sc = new Scanner(); <<객체
+	//클래스는 설계도 / 객체는..음
+	
+	private int nai = 80; //다른 클래스에서 수정하거나 접근하지 못하게 된다. 이렇게 접근제어자를 이용하여 권한을 주는 것을'정보 은닉화'라고 한다.(숨기는것 x)
+	//but.공개된 메서드로 허락을 받아서 쓰려면 getter setter(직접접근을 막는 방법)
+	//getter setter를 사용하면 직접접근은 불가능하지만 메소드를 이용해 접근할 수 있게된다.
+	public String gabo = "삼강청자";
+	protected String gahoon = "모든 일에 최선을"; //protected를 씀으로, 우리 가족만 쓸 수 있다. 이말임
+	
+	//각각의 객체에 접근 제어자를 사용하여 정보은닉을 함. 이런 개객체들이 모여있는  클래스를 '캡슐화'라고 함.
+	
+	public int getNai() { //나이는 못바꾸고 알려주기만 할 수 있어진다.
+		return nai;
+	}
+
+//	public int getNai() {
+//		return nai;
+//	}
+//
+//	public void setNai(int nai) { //set을 삭제하면 나이 바꿀수 있는 권한을 없애기 때문에 getter만 사용이 가능해짐. 이런 과정으로 나이를 바꾸지 않고 return만 할 수 있게 된다.
+//		this.nai = nai;
+//	}
+	
+	
+	
+}
---
+++ src/클래스_객체_정보은닉화/MyFamily.java
@@ -0,0 +1,15 @@
+package 클래스_객체_정보은닉화;
+
+public class MyFamily {
+
+	public static void main(String[] args) {
+		
+		GrandFa gr; //이건 클래스 (new를 하지 않았기 떄문에)
+		gr = new GrandFa(); //이건 객체(new를 했으므로 객체가 된다.)
+//		gr.setNai(85); //이렇게 해서 할아버지의 나이에 접근할 수 있게된다.(메소드를 사용하여)
+		System.out.println(gr.getNai());
+		
+
+	}
+
+}
