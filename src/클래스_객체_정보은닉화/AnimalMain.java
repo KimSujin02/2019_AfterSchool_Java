04054c7091c19e0cda9e8a3ccdcc71b5f28cf4f2
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-19 16:27:16 +0900

    0917

--- src/클래스_객체_정보은닉화/AnimalMain.java
+++ src/클래스_객체_정보은닉화/AnimalMain.java
@@ -2,27 +2,38 @@ package 클래스_객체_정보은닉화;
 
 public class AnimalMain {
 	public static void main(String args[]) {
-		
+//		
 		Ani_Cow cow = new Ani_Cow();
-		System.out.println(cow.name() + "는 " + cow.action() + "에 " + cow.eat() + "을 먹음");
-		System.out.println();
-		
-		
+//		System.out.println(cow.name() + "는 " + cow.action() + "에 " + cow.eat() + "을 먹음");
+//		System.out.println();
+//		
+//		
 		Ani_Lion lion = new Ani_Lion();
-		System.out.println(lion.name() + "는 " + lion.action() + "에 " + lion.eat() + "을 먹음");
-		System.out.println();
-		System.out.println("======별도의 클래스 사용======");
-		System.out.println();
-		
+//		System.out.println(lion.name() + "는 " + lion.action() + "에 " + lion.eat() + "을 먹음");
+//		System.out.println();
+//		System.out.println("======별도의 클래스 사용======");
+//		System.out.println();
+//		
 		AnimalFind af = new AnimalFind();
 		af.find(cow);
-		af.find(lion);
+//		af.find(cow);
+//		af.find(lion);
+//		
+//		Ani_Dog_Wolf dw = new Ani_Dog_Wolf();
+//		
+//		dw.special();
 		
-		Ani_Dog_Wolf dw = new Ani_Dog_Wolf();
-		
-		dw.special();
-		
-		
+//		Ani_Cow cow = new Ani_Cow();
+//		
+//		AnimalFind af = new AnimalFind();
+//		af.find(cow);
+//		
+//		Ani_Lion lion = new Ani_Lion();
+//		af.find(lion);
+//		
+//		Ani_Dog_Wolf dog= new Ani_Dog_Wolf();
+//		af.find(dog);
+					
 		
 		
 	}//main
