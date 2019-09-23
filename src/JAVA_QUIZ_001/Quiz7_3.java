960e6358d755250f5190665d9f136ba7aec6836b
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-23 17:17:42 +0900

    업캐스팅 다형성 QUIZ

--- src/JAVA_QUIZ_001/Quiz7_3.java
+++ src/JAVA_QUIZ_001/Quiz7_3.java
@@ -8,15 +8,15 @@ public class Quiz7_3 {
 	}
 	
 	public static void polyMethod(Shape ref) {
-		
+		ref.area();
 		if(ref instanceof Circle) {
-			System.out.println("반지름이  5인 원의 면적은 " + ref.area());
+			System.out.println("반지름이  5인 원의 면적은 " + ref.res); //ref.area()해도 됨....
 		}
 		else if(ref instanceof Rectangle) {
-			System.out.println("넓이가 10이고, 높이가 10인 사각형의 면적은 " + ref.area());
+			System.out.println("넓이가 10이고, 높이가 10인 사각형의 면적은 " + ref.res);
 		}
 		else if(ref instanceof Triangle) {
-			System.out.println("넓이가 10이고, 높이가 10인 삼각형의 면적은 " + ref.area());
+			System.out.println("넓이가 10이고, 높이가 10인 삼각형의 면적은 " + ref.res);
 		}
 		
 	}
