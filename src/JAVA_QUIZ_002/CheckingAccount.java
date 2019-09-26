7052a15d15534036676f0b51e685e976d0551441
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-26 17:15:17 +0900

    은행 계좌 개설, 직쿨카드로 은행업무처리 (다향성, 클래스, 메소드)

---
+++ src/JAVA_QUIZ_002/CheckingAccount.java
@@ -0,0 +1,34 @@
+package JAVA_QUIZ_002;
+
+public class CheckingAccount extends Account {
+	String cardNo;
+	
+	public CheckingAccount() {
+	}
+	
+	public CheckingAccount(String accountNo, String ownerName,int balance, String cardNo) {
+		super.accountNo = accountNo;
+		super.ownerName = ownerName;
+		super.balance = balance;
+		this.cardNo = cardNo;
+	}
+	
+	int pay(String cardNo, int amount) {
+		if(cardNo.equals(this.cardNo)) {
+			if(amount <= balance) {
+				balance -= amount;
+				System.out.println("지불액 : " + amount);
+			}
+			else {
+				System.out.println("지불이 불가능합니다.");
+				amount = 0;
+				System.out.println("지불액 : " + amount);
+			}
+		}
+		else {
+			System.out.println("카드번호가 틀렸습니다.");
+		}
+		return amount;
+	}
+	
+}
